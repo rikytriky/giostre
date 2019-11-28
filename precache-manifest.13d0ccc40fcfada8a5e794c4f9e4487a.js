007855e953a583ff7068b83c98f63fee76631f04
rikytriky <58169927+rikytriky@users.noreply.github.com>
2019-11-28 10:28:00 +0100

    Updates

---
+++ precache-manifest.13d0ccc40fcfada8a5e794c4f9e4487a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770ca4cfb0cb5dcb1a844f695d8ae351",
+    "url": "/giostre/index.html"
+  },
+  {
+    "revision": "050b3d349c3dfef38690",
+    "url": "/giostre/static/css/main.af62fe61.chunk.css"
+  },
+  {
+    "revision": "fb805b8fd347e01ff18e",
+    "url": "/giostre/static/js/2.4a1c42e0.chunk.js"
+  },
+  {
+    "revision": "050b3d349c3dfef38690",
+    "url": "/giostre/static/js/main.8cbf5858.chunk.js"
+  },
+  {
+    "revision": "c1f8b3f8b7d2a6aa183a",
+    "url": "/giostre/static/js/runtime-main.be5a0e19.js"
+  }
+]);
